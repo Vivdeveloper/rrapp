a8eea72d07df0a0fa13ea134a4c9e632ec4a15e8
Swapnilghadigaonkar <swapnil@hybrowlabs.in>
2024-09-02 17:14:53 +0530

    Update lead.js

--- rrchem/public/js/lead.js
+++ rrchem/public/js/lead.js
@@ -1,9 +1,9 @@
 frappe.ui.form.on('Lead', {
     refresh: function(frm) {
         if (frm.doc.company) {
-            // Call the server-side method to set the default company
+            
             frappe.call({
-                method: "frappe.core.doctype.session_default_settings.session_default_settings.set_session_default_values",  // Update with your actual app path
+                method: "frappe.core.doctype.session_default_settings.session_default_settings.set_session_default_values",  
                 args: {
                     default_values: {"company":frm.doc.company}
                 },
@@ -16,3 +16,27 @@ frappe.ui.form.on('Lead', {
         }
     }
 });
+
+frappe.provide("erpnext");
+cur_frm.email_field = "email_id";
+
+erpnext.LeadController = class LeadController extends frappe.ui.form.Controller {
+    refresh() {
+        let doc = this.frm.doc;
+
+        // Add Quotation button if the Lead is not new and not already a customer
+        if (!this.frm.is_new() && doc.__onload && !doc.__onload.is_customer) {
+            this.frm.add_custom_button(__("Trial"), this.make_quotation.bind(this), __("Create"));
+        }
+    }
+
+    make_quotation() {
+        frappe.model.open_mapped_doc({
+            method: "rrchem.override.make_quotation.make_quotation",  
+            frm: this.frm,
+        });
+    }
+};
+
+
+extend_cscript(cur_frm.cscript, new erpnext.LeadController({ frm: cur_frm }));
